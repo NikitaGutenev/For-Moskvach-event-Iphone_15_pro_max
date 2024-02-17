38e2654fd127b6c31c8de67410dd65fbd5497e1c
Harpy <gutenev229@gmail.com>
2024-02-17 22:32:17 +0300

    добавил принты

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ async def main():
                               text='Хочу айфон.',
                               reply_to_message_id=MAIN_MESSAGE)
         logging.info(f'{msg.link} | {msg.id} | Отослал сообщение №{count}')
+        print(f'{msg.link} | {msg.id} | Отослал сообщение №{count}')
         count += 1
         time_range = random.randint(58, 63)
         await asyncio.sleep(time_range)
@@ -30,11 +31,13 @@ async def main():
                                             message_ids=msg.id+1)
         if next_msg.from_user is None:
             logging.warning(f'Спустя минуту после отправки {msg.link} | {msg.id} нет сообщений!')
+            print(f'Спустя минуту после отправки {msg.link} | {msg.id} нет сообщений!')
             await asyncio.sleep(30)
             next_msg = await app_user.get_messages(chat_id=CHAT,
                                             message_ids=msg.id+1)
             if next_msg.from_user is None:
                 logging.warning(f'Спустя 30 сек до сих пор нет сообщений!')
+                print(f'Спустя 30 сек до сих пор нет сообщений!')
                 await asyncio.sleep(30)
                 next_msg = await app_user.get_messages(chat_id=CHAT,
                                                 message_ids=msg.id+1)
@@ -43,6 +46,7 @@ async def main():
                               text='УрааААаааААААаАа я победил! Мой прошлый коммент продержался больше 1 минуты последним!',
                               reply_to_message_id=MAIN_MESSAGE)
                     logging.critical(f'ТЫ ВЫИГРАЛ!!!! Ссылка на сообщение (последнее): {msg.link} | {msg.id}\nСсылка на сообщение(победное): {win.link} | {win.id}')
+                    print(f'ТЫ ВЫИГРАЛ!!!! Ссылка на сообщение (последнее): {msg.link} | {msg.id}\nСсылка на сообщение(победное): {win.link} | {win.id}')
                     break
     await app_user.stop()
 
