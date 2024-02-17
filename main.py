8d059546bc2d3542db6ad77f3797907bc3e3bbf2
Harpy <gutenev229@gmail.com>
2024-02-17 23:12:12 +0300

    пофиксил бота (улучшил скорее)

--- main.py
+++ main.py
@@ -27,21 +27,18 @@ async def main():
         count += 1
         time_range = random.randint(58, 63)
         await asyncio.sleep(time_range)
-        next_msg = await app_user.get_messages(chat_id=CHAT,
-                                            message_ids=msg.id+1)
-        if next_msg.from_user is None:
+        next_msg = [i async for i in app_user.get_chat_history(chat_id=CHAT, limit=1)][0]
+        if next_msg.id == msg.id:
             logging.warning(f'Спустя минуту после отправки {msg.link} | {msg.id} нет сообщений!')
             print(f'Спустя минуту после отправки {msg.link} | {msg.id} нет сообщений!')
             await asyncio.sleep(30)
-            next_msg = await app_user.get_messages(chat_id=CHAT,
-                                            message_ids=msg.id+1)
-            if next_msg.from_user is None:
+            next_msg = [i async for i in app_user.get_chat_history(chat_id=CHAT, limit=1)][0]
+            if next_msg.id == msg.id:
                 logging.warning(f'Спустя 30 сек до сих пор нет сообщений!')
                 print(f'Спустя 30 сек до сих пор нет сообщений!')
                 await asyncio.sleep(30)
-                next_msg = await app_user.get_messages(chat_id=CHAT,
-                                                message_ids=msg.id+1)
-                if next_msg.from_user is None:
+                next_msg = [i async for i in app_user.get_chat_history(chat_id=CHAT, limit=1)][0]
+                if next_msg.id == msg.id:
                     win = await app_user.send_message(chat_id=CHAT,
                               text='УрааААаааААААаАа я победил! Мой прошлый коммент продержался больше 1 минуты последним!',
                               reply_to_message_id=MAIN_MESSAGE)
@@ -52,4 +49,4 @@ async def main():
 
 if __name__ == '__main__':
     app_user = ClientP(name='Pyrogram_Client', api_id=os.environ['API_ID'], api_hash=os.environ['API_HASH'])
-    asyncio.run(main())
+    app_user.run(main())
